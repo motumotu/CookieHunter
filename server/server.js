83ca041c874624f2ad4444da060741531ae6befa
motumotu <s13t270@stmail.eng.kagawa-u.ac.jp>
2015-11-19 13:31:07 +0900

    接続処理追加

--- server/server.js
+++ server/server.js
@@ -1,19 +1,15 @@
-var http = require('http');
-var fs = require('fs');
-var settings = require('../config/settings');
-var server = http.createServer();
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-server.on('request', function(req, res) {
-    fs.readFile('../client/index.html', 'utf-8', function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.write('page not found');
-            return res.end();
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-    });
+app.use(express.static(__dirname + '/../client'));
+
+io.on('connection', function(socket) {
+    console.log("client connected");
 });
-server.listen(settings.port, settings.host);
-console.log("server listening");
+
+http.listen(3000, function() {
+    console.log("server listen");
+});
+
