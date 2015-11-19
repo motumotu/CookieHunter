9a9e84538720210a1a6517acb442853e5ea099b7
motumotu <s13t270@stmail.eng.kagawa-u.ac.jp>
2015-11-19 15:25:55 +0900

    プレイヤー座標の同期

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,130 @@
 $(function() {
-    //var io = require('socket.io-client');
-    container = document.getElementById("canvasContainer");
-    canvas = document.getElementById("canvas");
-    context = canvas.getContext("2d");
-    context.fillRect(10, 10, 50, 50);
+    const MAX_PLAYER = 100;
 
+    //var io = require('socket.io-client');
+    var container = document.getElementById("canvasContainer");
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var imageMap;
+    var imagePlayer;
+    var wait = 100;
+    var my;
+    var queue = null;
     var socket = io.connect();
+    var players = [];
+    /*var playerState = {
+        id: 0,
+        x: 0,
+        y: 0
+    };
+    */
+    var PlayerState = function() {
+        this.id = 0;
+        this.x = 0;
+        this.y = 0;
+    }
+    var MyState = function() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    window.onload = function() {
+        init();
+        setInterval(function() {
+            update();
+            draw();
+        }, 16);
+        setInterval(function() {
+            emitServer();
+        }, 1000);
+    }
+
+    function emitServer() {
+        socket.emit('update', my);
+    }
+
+    window.addEventListener("resize", function() {
+        clearTimeout(queue);
+        queue = setTimeout(function() {
+            settingCanvas();
+        }, wait);
+    }, false);
+
+    function settingCanvas() {
+        $('#canvas').attr('width', $('#canvasContainer').width());
+        $('#canvas').attr('height', $('#canvasContainer').height());
+    }
+    
+    $("canvas").mousedown(function(e) {
+        var mousex = e.clientX;
+        var mousey = e.clientY;
+        my.x = mousex;
+        my.y = mousey;
+    });
+    
+    socket.on('add_player', function(data) {
+        console.log("add_player "+data.id+" "+data.x+" "+data.y);
+        var n = data.id;
+        players[n].id = n;
+        players[n].x = data.x;
+        players[n].y = data.y;
+    });
+    
+    socket.on('update_players', function(data) {
+        console.log("update_players "+data.id+" "+data.x+" "+data.y);
+        //players[data.id] = data.id;
+        players[data.id].x = data.x;
+        players[data.id].y = data.y;
+    });
+
+    function init() {
+        settingCanvas();
+        loadImage();
+        my = new MyState();
+        for (var i = 0; i < MAX_PLAYER; i++) {
+            players[i] = new PlayerState();
+        }
+    }
+
+    function update() {
+
+    }
+
+    function draw() {
+        context.fillStyle="rgba(255,255,255,1)";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        drawMap();
+        drawMy();
+        drawPlayer();
+    }
+
+    function loadImage() {
+        imageMap = new Image();
+        imagePlayer = new Image();
+        imageMap.src = "image/map1.png";
+        imagePlayer.src = "image/player.png";
+    }
+
+    function drawMap() {
+        for (var y = 0; y < 50; y++) {
+            for (var x = 0; x < 50; x++) {
+                var px = 40 * x;
+                var py = 40 * y;
+                if (px > canvas.width) break;
+                if (py > canvas.height) break;
+                context.drawImage(imageMap, 40 * x, 40 * y);
+            }
+        }
+    }
+
+    function drawMy() {
+        context.drawImage(imagePlayer, my.x, my.y);
+    }
+    function drawPlayer() {
+        for (var i = 0; i < MAX_PLAYER; i++) {
+            if (players[i].id == 0) continue;
+            console.log("draw "+players[i].x+" "+players[i].y);
+            context.drawImage(imagePlayer, players[i].x, players[i].y);
+        }
+    }
 })
--- server/server.js
+++ server/server.js
@@ -7,11 +7,25 @@ var settings = require(__dirname + '/../config/settings');
 
 app.use(express.static(__dirname + '/../client'));
 
-io.on('connection', function(socket) {
+io.sockets.on('connection', function(socket) {
     console.log("client connected");
+    socket.on('update', function(data) {
+        console.log("update "+data.x+" "+data.y);
+        socket.broadcast.emit('update_players', {
+            id: 1,
+            x: data.x,
+            y: data.y
+               
+       });
+    });
+    io.sockets.emit('add_player', {
+        id: 1,
+        x: 100,
+        y: 100
+    });
 });
 
 http.listen(settings.port, function() {
     console.log("server listen");
-});
+})
 
